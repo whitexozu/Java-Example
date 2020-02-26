0e095152d3d97f57a753670e484d5c80e8e0c10b
kim jihoon <whitexozu@gmail.com>
2020-02-27 05:14:20 +0900

    .

--- bin/execute/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/Users/whitexozu/dev/Apps/spark-2.1.0-bin-hadoop2.7/bin/spark-submit /Users/whitexozu/dev/pycharm_workspace/pythonExample/spark/extractionIVTable/createIVTable.py yarn 201709
